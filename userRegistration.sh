dc4d4bab1ed133848540bde8a5606c746eb86276
Akshay Barve <61134490+akshaybarve06@users.noreply.github.com>
2020-03-17 18:45:34 +0530

    Added: Email Validation, Req. Fields

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,30 +1,52 @@
 #!/bin/bash -x
+shopt -s extglob
 
 # @ Description: User Registration Problem
 # @ Author: Akshay Dhananjay Barve
 # @ Version: 18.04.3 lts
 # @ Since: 17th March 2020 / Tuesday
 
-checkDetails()
-{
 read -p "Enter First Name " firstname
 read -p "Enter Last Name " lastname
+read -p "Enter Email-id " email
 
-pattern="^[A-Z][a-z]{2,}$"
+namePattern="^[A-Z][a-z]{2,}$"
 
-if [[ $firstname =~ $pattern ]]
-then
-   echo Valid First Name
-else
-   echo Invalid First Name
-fi
+emailPattern="^[a-zA-Z]{1,}([.]?[a-zA-Z]{1,})?[@]{1}[a-zA-Z]{1,}[.]{1}[a-z]{2,3}([.]?[a-z]{2})?$"
 
-if [[ $lastname =~ $pattern ]]
-then
-   echo Valid Last Name
-else
-   echo Invalid Last Name
-fi
+checkFirstName()
+{
+	firstname=$1
 
+	if [[ $firstname =~ $namePattern ]]
+	then
+		echo Valid First Name
+	else
+		echo Invalid First Name
+	fi
 }
-checkDetails
+checkLastName()
+{
+	lastname=$1
+
+   if [[ $lastname =~ $namePattern ]]
+   then
+      echo Valid Last Name
+   else
+      echo Invalid Last Name
+   fi
+}
+checkEmail()
+{
+	email=$1
+
+	if [[ $email =~ $emailPattern ]]
+	then
+   	echo Valid Email-Id
+	else
+   	echo Invalid Email-Id
+	fi
+}
+checkFirstName $firstname
+checkLastName $lastname
+checkEmail $email
